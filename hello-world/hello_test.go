2111ec504302290bd38a464928de8a6907b2241f
Tocyuki <tocguitar1@gmail.com>
2024-12-09 16:09:18 +0900

    fix Hello()

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -10,13 +10,13 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
