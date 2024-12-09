e5a861c81f439ec42f6e909df23ade3f2f38bf05
Tocyuki <tocguitar1@gmail.com>
2024-12-09 15:27:53 +0900

    move link & create README

---
+++ hello-world/README.md
@@ -0,0 +1,4 @@
+# Hello, World
+
+https://andmorefine.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world
+
--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,5 +1,3 @@
-// https://andmorefine.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world
-
 package main
 
 import "fmt"
