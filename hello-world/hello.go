989ab594b71352a22374b69b51f947342b6e0326
Tocyuki <tocguitar1@gmail.com>
2024-12-10 14:17:29 +0900

    refactor Hello()

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -13,16 +13,18 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
-		prefix = spanishHelloPrefix
+		return spanishHelloPrefix
 	case french:
-		prefix = frenchHelloPrefix
+		return frenchHelloPrefix
+	default:
+		return englishHelloPrefix
 	}
-
-	return prefix + name
 }
 
 func main() {
