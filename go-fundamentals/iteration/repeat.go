f6ea5ba97a74ae3c158ef3f1c0479763c5a3baca
Tocyuki <tocguitar1@gmail.com>
2024-12-15 18:03:13 +0900

    add ExampleRepeat()

--- go-fundamentals/iteration/repeat.go
+++ go-fundamentals/iteration/repeat.go
@@ -1,9 +1,17 @@
 package iteration
 
+import "fmt"
+
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 	return repeated
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
