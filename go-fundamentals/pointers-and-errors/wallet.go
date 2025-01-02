40e48d0313c4361b4cd4a89ae8c3465b2f2c318b
Tocyuki <tocguitar1@gmail.com>
2025-01-02 16:13:40 +0900

    check address of balance

--- go-fundamentals/pointers-and-errors/wallet.go
+++ go-fundamentals/pointers-and-errors/wallet.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Dposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
