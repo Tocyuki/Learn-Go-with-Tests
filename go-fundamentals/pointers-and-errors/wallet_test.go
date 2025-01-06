0ce77a2bafd53d39322419707d4c2427db2300de
Tocyuki <tocguitar1@gmail.com>
2025-01-06 22:21:34 +0900

    Added a test to ensure that Withdraw() does not return an error if successful

--- go-fundamentals/pointers-and-errors/wallet_test.go
+++ go-fundamentals/pointers-and-errors/wallet_test.go
@@ -12,10 +12,12 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
-		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+	t.Run("Withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -44,5 +46,12 @@ func assertError(t *testing.T, got error, want error) {
 
 	if !errors.Is(got, want) {
 		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("didn't get an error but wanted one")
 	}
 }
